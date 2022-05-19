919d6b01eb29897f296a3907f76f6cf47a5b9ac4
Samantha Crespo <samkcrespo@gmail.com>
2022-05-19 14:40:15 -0700

    commit tree dd10cf73883ea8da72054719c9a129d6807d5683

---
+++ lib/concerns.rb
@@ -0,0 +1,16 @@
+module Memorable
+module ClassMethods
+  def self.reset_all
+    self.all.clear
+  end
+
+  def self.count
+    self.all.count
+  end
+end
+ module InstanceMethods
+    def initialize
+      self.class.all << self
+    end
+  end
+end
